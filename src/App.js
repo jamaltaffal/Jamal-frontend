7be4a455c78739915e7bbb03e9cb0c82f0d75269
jamaltaffal <jamal.taffal@optimumpartners.co>
2026-02-17 11:24:33 +0300

    Update API_URL to use new endpoint

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import './App.css';
 
-const API_URL = process.env.REACT_APP_API_URL || 'http://37.27.31.43:4000';
+const API_URL = process.env.REACT_APP_API_URL || 'https://devops-1.sociumtech.com';
 
 function App() {
   const [tasks, setTasks] = useState([]);
